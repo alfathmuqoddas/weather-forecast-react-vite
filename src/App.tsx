e7599e210571e375d0bba48eb53bf59d7a04f3bc
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-04-10 16:23:04 +0700

    update: adjust loading state

--- src/App.tsx
+++ src/App.tsx
@@ -63,8 +63,6 @@ export default function App() {
     return <div>Failed to load.</div>;
   }
 
-  if (isLoading) return <>Loading...</>;
-
   return (
     <div
       style={{
@@ -79,116 +77,122 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <WeatherCard>
-        <LeftSide>
-          <MainWeather
-            mainDate=""
-            bigIcon={weather?.list[2].weather[0].icon || ""}
-            mainTemp={weather?.list[2].main.temp.toFixed(0) || ""}
-            feels={weather?.list[2].main.feels_like.toFixed(0) || ""}
-            min={weather?.list[2].main.temp_min.toFixed(0) || ""}
-            max={weather?.list[2].main.temp_max.toFixed(0) || ""}
-            mainDesc={weather?.list[2].weather[0].description || ""}
-            humidity={weather?.list[2].main.humidity || ""}
-            city={weather?.city.name || ""}
-            country={weather?.city.country || ""}
-            handleSubmitLocation={handleSubmit}
-          />
-        </LeftSide>
-        <RightSide>
-          <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
-            {weeks.map((week, index) => (
-              <DailyCard
-                key={index}
-                day={dayjs.unix(weather?.list[week]?.dt || 0).format("ddd")}
-                temp={weather?.list[week].main.temp.toFixed(0) || ""}
-                feels={weather?.list[week].main.feels_like.toFixed(0) || ""}
-                icon={weather?.list[week].weather[0].icon || ""}
+      {error ? (
+        <>Error Please Try Again Later</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : (
+        <WeatherCard>
+          <LeftSide>
+            <MainWeather
+              mainDate=""
+              bigIcon={weather?.list[2].weather[0].icon || ""}
+              mainTemp={weather?.list[2].main.temp.toFixed(0) || ""}
+              feels={weather?.list[2].main.feels_like.toFixed(0) || ""}
+              min={weather?.list[2].main.temp_min.toFixed(0) || ""}
+              max={weather?.list[2].main.temp_max.toFixed(0) || ""}
+              mainDesc={weather?.list[2].weather[0].description || ""}
+              humidity={weather?.list[2].main.humidity || ""}
+              city={weather?.city.name || ""}
+              country={weather?.city.country || ""}
+              handleSubmitLocation={handleSubmit}
+            />
+          </LeftSide>
+          <RightSide>
+            <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
+              {weeks.map((week, index) => (
+                <DailyCard
+                  key={index}
+                  day={dayjs.unix(weather?.list[week]?.dt || 0).format("ddd")}
+                  temp={weather?.list[week].main.temp.toFixed(0) || ""}
+                  feels={weather?.list[week].main.feels_like.toFixed(0) || ""}
+                  icon={weather?.list[week].weather[0].icon || ""}
+                />
+              ))}
+            </div>
+            <HighlightWrapper>
+              <HighlightCard
+                title="Sunrise & Sunset ⛅"
+                body=""
+                unit=""
+                body2={
+                  <p className="font-bold">
+                    🌄
+                    {getSunrise(
+                      weather?.city?.sunrise || 0,
+                      weather?.city?.timezone || 0
+                    )}
+                  </p>
+                }
+                body3={
+                  <p className="font-bold">
+                    🌇
+                    {getSunset(
+                      weather?.city?.sunset || 0,
+                      weather?.city?.timezone || 0
+                    )}
+                  </p>
+                }
               />
-            ))}
-          </div>
-          <HighlightWrapper>
-            <HighlightCard
-              title="Sunrise & Sunset ⛅"
-              body=""
-              unit=""
-              body2={
-                <p className="font-bold">
-                  🌄
-                  {getSunrise(
-                    weather?.city?.sunrise || 0,
-                    weather?.city?.timezone || 0
-                  )}
-                </p>
-              }
-              body3={
-                <p className="font-bold">
-                  🌇
-                  {getSunset(
-                    weather?.city?.sunset || 0,
-                    weather?.city?.timezone || 0
-                  )}
-                </p>
-              }
-            />
 
-            <HighlightCard
-              title="Humidity 💧"
-              body={weather?.list[2]?.main.humidity || 0}
-              body2=""
-              body3=""
-              unit="%"
-            />
+              <HighlightCard
+                title="Humidity 💧"
+                body={weather?.list[2]?.main.humidity || 0}
+                body2=""
+                body3=""
+                unit="%"
+              />
 
-            <HighlightCard
-              title="Wind Speed 🪁"
-              body={Math.round((weather?.list[2].wind.speed ?? 0) * 3.6)}
-              unit="KM/h"
-              body2=""
-              body3=""
-            />
+              <HighlightCard
+                title="Wind Speed 🪁"
+                body={Math.round((weather?.list[2].wind.speed ?? 0) * 3.6)}
+                unit="KM/h"
+                body2=""
+                body3=""
+              />
 
-            <HighlightCard
-              title="Pressure 🌊"
-              body={weather?.list[2].main.pressure || 0}
-              body2=""
-              body3=""
-              unit="hPa"
-            />
+              <HighlightCard
+                title="Pressure 🌊"
+                body={weather?.list[2].main.pressure || 0}
+                body2=""
+                body3=""
+                unit="hPa"
+              />
 
-            <HighlightCard
-              title="Visibility 🕶"
-              body={(weather?.list[2].visibility ?? 0) / 1000}
-              unit="KM"
-              body2=""
-              body3=""
-            />
-            <HighlightCard
-              title="API From 🗺"
-              body=""
-              unit=""
-              body3=""
-              body2={
-                <div className="">
-                  <a
-                    href="https://openweathermap.org"
-                    target="_blank"
-                    className="hover:underline"
-                  >
-                    OpenWeatherMap
-                  </a>
-                  <button
-                    className="mt-2 bg-blue-500 rounded-full px-2 py-1 text-white hover:cursor-pointer"
-                    onClick={handleRandomizeClick}
-                  >
-                    Randomize Background
-                  </button>
-                </div>
-              }
-            />
-          </HighlightWrapper>
-        </RightSide>
-      </WeatherCard>
+              <HighlightCard
+                title="Visibility 🕶"
+                body={(weather?.list[2].visibility ?? 0) / 1000}
+                unit="KM"
+                body2=""
+                body3=""
+              />
+              <HighlightCard
+                title="API From 🗺"
+                body=""
+                unit=""
+                body3=""
+                body2={
+                  <div className="">
+                    <a
+                      href="https://openweathermap.org"
+                      target="_blank"
+                      className="hover:underline"
+                    >
+                      OpenWeatherMap
+                    </a>
+                    <button
+                      className="mt-2 bg-blue-500 rounded-full px-2 py-1 text-white hover:cursor-pointer"
+                      onClick={handleRandomizeClick}
+                    >
+                      Randomize Background
+                    </button>
+                  </div>
+                }
+              />
+            </HighlightWrapper>
+          </RightSide>
+        </WeatherCard>
+      )}
     </div>
   );
 }
