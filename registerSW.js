8c64a3543243271e5d8d2a0961205462402e3091
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-10-17 19:42:32 +0700

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/weather-forecast-react-vite/sw.js', { scope: '/weather-forecast-react-vite/' })})}
