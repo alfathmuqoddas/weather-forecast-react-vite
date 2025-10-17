deed31c33f820b693e0532a128d0564b923468b0
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-10-17 19:42:01 +0700

    update: add pwa features

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
   base: "/weather-forecast-react-vite/",
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Alfath's Weather Forecase",
+        short_name: "Weather Forecast",
+        description: "A Weather Forecast app built with React and Vite",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/weather-forecast-react-vite/",
+        start_url: "/weather-forecast-react-vite/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
